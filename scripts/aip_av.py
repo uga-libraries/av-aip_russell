7f907cc48655ea386950841c25307193e1750c32
Adriane Hanson <ahansonuga@gmail.com>
2021-04-15 10:41:34 -0400

    Add fuctions to aip_av.py
    Previously, each workflow step was a separate script that was called from aip_av.py using subprocess.run(). Now, each workflow step is a function.

--- scripts/aip_av.py
+++ scripts/aip_av.py
@@ -27,9 +27,134 @@ Script steps:
 
 import datetime
 import os
+import shutil
 import subprocess
 import sys
 from variables import move_error, scripts
+
+
+def move_error(error_name, item):
+    """Move the AIP folder to an error folder, named with the error, so the rest of the workflow steps are not run on
+    the AIP. """
+    if not os.path.exists(f'errors/{error_name}'):
+        os.makedirs(f'errors/{error_name}')
+    os.replace(item, f'errors/{error_name}/{item}')
+
+
+def aip_directory(aip):
+    """Makes the AIP directory structure (objects and metadata folder within the AIP folder) and moves the digital
+    objects to the objects folder. """
+
+    # Makes the objects folder within the AIP folder, if it doesn't exist. If there is already a folder named objects
+    # in the first level within the AIP folder, moves the AIP to an error folder and quits this script. Do not want
+    # to alter the original directory structure by adding to an original folder named objects.
+    try:
+        os.mkdir(f'{aip}/objects')
+    except FileExistsError:
+        move_error('preexisting_objects_folder', aip)
+        exit()
+
+    # Moves the contents of the AIP folder into the objects folder.
+    # Skips the objects folder because it is an error to try to move something into itself.
+    for item in os.listdir(aip):
+        if item == 'objects':
+            continue
+        os.replace(f'{aip}/{item}', f'{aip}/objects/{item}')
+
+    # Makes the metadata folder within the AIP folder.
+    # Do not have to check if it already exists since everything is moved to the objects folder in the previous step.
+    os.mkdir(f'{aip}/metadata')
+
+
+def mediainfo(aip, aip_type):
+    """Extracts technical metadata from the files in the objects folder using MediaInfo."""
+    # Runs MediaInfo on the contents of the objects folder and saves the xml output to the metadata folder.
+    # --'Output=XML' uses the XML structure that started with MediaInfo 18.03
+    # --'Language=raw' outputs the size in bytes.
+    subprocess.run(
+        f'mediainfo -f --Output=XML --Language=raw "{aip}/objects" > "{aip}/metadata/{aip}_{aip_type}_mediainfo.xml"',
+        shell=True)
+
+    # Copies mediainfo xml to a separate folder (mediainfo-xml) for staff reference.
+    # If a file by that name is already in mediainfo-xml,
+    #   moves the AIP to an error folder instead since the AIP may be a duplicate.
+    if os.path.exists(f'mediainfo-xml/{aip}_{aip_type}_mediainfo.xml'):
+        move_error('preexisting_mediainfo_copy', aip)
+    else:
+        shutil.copy2(f'{aip}/metadata/{aip}_{aip_type}_mediainfo.xml', 'mediainfo-xml')
+
+
+def preservation_xml(aip, aip_type):
+    """Creates PREMIS and Dublin Core metadata from the MediaInfo xml and saves it as a preservation.xml file."""
+    # Makes the preservation.xml file from the mediainfo.xml using a stylesheet and saves it to the AIP's metadata folder.
+    # If the mediainfo.xml is not present, moves the AIP to an error folder and quits this script.
+    mediainfo = f'{aip}/metadata/{aip}_{aip_type}_mediainfo.xml'
+    stylesheet = f'{stylesheets}/mediainfo-to-preservation.xslt'
+    preservationxml = f'{aip}/metadata/{aip}_{aip_type}_preservation.xml'
+
+    if os.path.exists(mediainfo):
+        subprocess.run(
+            f'java -cp "{saxon}" net.sf.saxon.Transform -s:"{mediainfo}" -xsl:"{stylesheet}" -o:"{preservationxml}" type={aip_type}',
+            shell=True)
+    else:
+        move_error('no_mediainfo_xml', aip)
+        exit()
+
+    # Validates the preservation.xml against the requirements of the UGA Libraries' digital preservation system (ARCHive).
+    # Possible validation errors:
+    #   preservation.xml was not made (failed to loaded)
+    #   preservation.xml does not match the metadata requirements (fails to validate)
+    validate = subprocess.run(f'xmllint --noout -schema "{stylesheets}/preservation.xsd" "{preservationxml}"',
+                              stderr=subprocess.PIPE, shell=True)
+
+    # If the preservation.xml isn't valid, moves the AIP to an error folder and saves the validation error to a text
+    # document in the error folder. If the preservation.xml is valid, copies the preservation.xml to local server for
+    # staff use.
+    if 'failed to load' in str(validate) or 'fails to validate' in str(validate):
+        move_error('preservation_invalid', aip)
+        with open(f'errors/preservation_invalid/{aip}_preservationxml_validation_error.txt', 'a') as error:
+            lines = str(validate.stderr).split('\\n')
+            for line in lines:
+                error.write(f'{line}\n\n')
+    else:
+        shutil.copy2(preservationxml, 'preservation-xml')
+
+
+def package(aip, aip_type):
+    """Bags, tars, and zips the AIP."""
+    # Deletes any .DS_Store files because they cause errors with bag validation.
+    # They would have been deleted in aip_av.py, but can be regenerated while the script is running.
+    for root, dirs, files in os.walk('.'):
+        for item in files:
+            if item == '.DS_Store':
+                os.remove(f'{root}/{item}')
+
+    # Bags the AIP folder in place.
+    # Both md5 and sha256 checksums are generated to guard against tampering.
+    subprocess.run(f'bagit.py --md5 --sha256 --quiet "{aip}"', shell=True)
+
+    # Renames the AIP folder to add the AIP type and '_bag' to the end.
+    # This is saved to a variable first since it is used a few more times in the script.
+    bag_name = f'{aip}_{aip_type}_bag'
+    os.replace(aip, bag_name)
+
+    # Validates the bag. If the bag is not valid, moves the AIP to an error folder, saves the validation error to a
+    # document in the error folder, and quits this script. The validation output is converted from a byte type to a
+    # string for easier formatting. The error document is formatted so each error is its own line.
+    validate = subprocess.run(f'bagit.py --validate --quiet "{bag_name}"', stderr=subprocess.PIPE, shell=True)
+
+    if 'invalid' in str(validate):
+        move_error('bag_invalid', bag_name)
+        with open(f'errors/bag_invalid/{aip}_bag_validation_error.txt', 'a') as error:
+            lines = str(validate.stderr).split(';')
+            for line in lines:
+                error.write(f'{line}\n\n')
+        exit()
+
+    # Tars and zips the AIP using a Perl script.
+    # The script also adds the uncompressed file size to the filename.
+    # The tarred and zipped AIP is saved to the aips-to-ingest folder.
+    subprocess.run(f'perl "{scripts}/prepare_bag" "{bag_name}" aips-to-ingest', shell=True)
 
 
 # Gets the AIPs directory from the script argument.
