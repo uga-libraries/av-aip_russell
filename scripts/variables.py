d806bdfefbeef4effcdc7b6fa0c9f0bbe982510a
Adriane Hanson <ahansonuga@gmail.com>
2021-04-26 09:50:01 -0400

    Updated saxon version in variables.py
    Downloaded the most recent version of Saxon, 10.1, for doing testing.

--- scripts/variables.py
+++ scripts/variables.py
@@ -1,6 +1,6 @@
 # Purpose: Defines variables there are specific to your local machine.
 
-# Script tested with SaxonHE9-9-1J.
-saxon = '/INSERT-PATH/saxon9he.jar'
+# Script tested with SaxonHE10.1.
+saxon = '/INSERT-PATH/saxon-he-10.1.jar'
 stylesheets = '/INSERT-PATH/stylesheets'
 prepare_bag = '/INSERT-PATH/prepare_bag'
