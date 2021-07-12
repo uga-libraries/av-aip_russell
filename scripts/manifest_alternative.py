5d6dc0fc51059e3d9b55d7d3ab792a65ccc29d0a
Adriane Hanson <ahansonuga@gmail.com>
2021-07-12 09:40:19 -0400

    Works with md5deep, manifest_alternative.py
    Got the logic working for using Python to iterative and the fixity tool to do one file at a time. Now just need to swap out the md5deep command for md5sum.

--- scripts/manifest_alternative.py
+++ scripts/manifest_alternative.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 
 # Extra code to get to the state that we would be after running the previous code in aip_av.py
-aips_directory = "C:/Users/amhan/Desktop/Test_Hargrett"
+aips_directory = "C:/Users/amhan/Desktop/Test_Russell"
 os.chdir(aips_directory)
 
 # Makes a MD5 manifest of all packaged AIPs for each department in the aips-to-ingest folder using md5sum.
@@ -17,24 +17,27 @@ os.chdir('aips-to-ingest')
 current_date = datetime.datetime.now().strftime("%Y-%m-%d-%H%M")
 if not len(os.listdir()) == 0:
     for file in os.listdir():
+
+        # Get fixity output for the file and reformat to be md5<tab>filename.
+        md5deep_output = subprocess.run(f"md5deep64 -b {file}", stdout=subprocess.PIPE, shell=True)
+        fixity = bytes.decode(md5deep_output.stdout).strip()
+
+        # Save the fixity to the correct department manifest.
         if file.startswith("har"):
-            fixity = subprocess.run(f"md5deep {file}")
-            print(fixity)
             with open(f"{current_date}_hargrett_manifest.txt", "a") as manifest:
-                manifest.write(f"{file}\n")
+                manifest.write(f"{fixity}\n")
         elif file.startswith("rbrl"):
             with open(f"{current_date}_russell_manifest.txt", "a") as manifest:
-                manifest.write(f"{file}\n")
+                manifest.write(f"{fixity}\n")
 else:
     print('\nCould not make manifest. aips-to-ingest is empty.')
 
 """
 Test Results:
 Correctly prints error if aips-to-ingest is empty.
-Correctly makes the hargrett and/or russell manifests and saves the filename to them.
+Correctly makes the hargrett and/or russell manifests and saves the filename(s) to them.
+Correctly makes manifests for all test cases with md5deep on Windows.
 
 To Do:
-Only Russell, Only Hargrett, and Mix of Both, one file at a time, with md5deep.
 Only Russell, Only Hargrett, and Mix of Both, one file at a time, with md5sum (Mac only).
-Compare results to correct manifest made with md5deep for the entire folder.
 """
