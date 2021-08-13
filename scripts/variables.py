995599577ea2b9243596625c71d20a0e6563cfb6
Adriane Hanson <ahansonuga@gmail.com>
2021-08-13 15:11:50 -0400

    Update variables.py
    Added explanation of where this file is used and removed "/" before INSERT-PATH to emphasize it is the absolute and not relative path needed.

--- scripts/variables.py
+++ scripts/variables.py
@@ -1,6 +1,7 @@
 # Purpose: Defines variables there are specific to your local machine.
+# These are imported by aip_av.py
 
 # Script tested with SaxonHE10.1.
-saxon = '/INSERT-PATH/saxon-he-10.1.jar'
-stylesheets = '/INSERT-PATH/stylesheets'
-prepare_bag = '/INSERT-PATH/prepare_bag'
+saxon = 'INSERT-PATH/saxon-he-##.#.jar'
+stylesheets = 'INSERT-PATH/stylesheets'
+prepare_bag = 'INSERT-PATH/prepare_bag'
